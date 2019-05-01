f923286fafdff8b7439aceb596daa59140bbee67
Wei Alex <lexnewgate@gmail.com>
2019-05-01 23:21:28 +0800

    Update Program.cs

--- 14 ConcurrencyAndAsync/Threading/06 Exception Handling/Program.cs
+++ 14 ConcurrencyAndAsync/Threading/06 Exception Handling/Program.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
+/*
+*
+* Any try/catch/finally blocks in effect when a thread is created are of 
+* no relevance to the thread when it starts executing. 
+* 
+* This behavior makes sense when you consider that each thread has an 
+* independent execution path.
+*/
+
 
 namespace _06_Exception_Handling
 {
@@ -10,7 +21,32 @@ namespace _06_Exception_Handling
     {
         static void Main(string[] args)
         {
+            new Thread(() =>
+            {
+                try
+                {
+                    Go();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("exception");
+                }
+            }).Start();
+            //try
+            //{
+            //    new Thread(Go).Start();
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine("exception");
+            //}
+        }
 
+
+
+        static void Go()
+        {
+            throw null;
         }
     }
 }
